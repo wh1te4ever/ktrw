73bc212af1883f4dd7bfa97b356072046c3563c8
wh1te4ever <shg1725@nate.com>
2025-02-22 13:01:32 +0900

    Add some scripts for convenience

---
+++ load_kext.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+pongo_kext_loader/pongo_kext_loader \
+    -l pongo_kextload/kextload.pongo-module \
+    -k ktrw_gdb_stub/ktrw_gdb_stub.ikext
---
+++ set_proxy.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+ktrw_usb_proxy/ktrw_usb_proxy 39399;
